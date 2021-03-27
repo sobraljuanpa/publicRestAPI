8117b4dcf314f0c325afd7caa4c2f08e69088ad4
Juan Pablo Sobral <sobraljuanpa@gmail.com>
2021-03-27 16:10:35 -0300

    Defined Repository interface and DB Context class

---
+++ IDataAccess/Context.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Domain;
+
+namespace IDataAccess
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Playlist> Playlists { get; set; }
+        public DbSet<PlayableContent> PlayableContents { get; set; }
+    }
+}
---
+++ IDataAccess/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IDataAccess
+{
+    public interface IRepository<T>
+    {
+        IEnumerable<T> GetAll();
+        T Get(int id);
+        void Add(T entity);
+        void Update(T dbEntity, T newEntity);
+        void Delete(T entity);
+    }
+}
