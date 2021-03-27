6e6d76f500fbbc1e2a37063326743943687a2685
Juan Pablo Sobral <sobraljuanpa@gmail.com>
2021-03-27 13:18:02 -0300

    Added CategoryElement class, PlayableContent class and tests

---
+++ Domain/CategoryElement.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Domain
+{
+    public abstract class CategoryElement
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string ImageURL { get; set; }
+    }
+}
