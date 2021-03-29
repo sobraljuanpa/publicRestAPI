3d9add3de46e9edf74cd9655369b501577983e3c
Chiara <chiara_dimarco@hotmail.com>
2021-03-29 18:44:17 -0300

    Added Consultation class and tests

---
+++ Domain/Consultation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public class Consultation
+    {
+        public int Id { get; set; }
+
+        public string PatientName { get; set; }
+
+        public DateTime PatientBirthDate { get; set; }
+
+        public string PatientEmail { get; set; }
+
+        public string PatientPhone { get; set; }
+    }
+}
