c7fb2439c65ff6afb965189fd3a27f7b81e17c41
Juan Pablo Sobral <sobraljuanpa@gmail.com>
2021-04-01 17:40:29 -0300

    Added administratorBL interface

---
+++ IBusinessLogic/IAdministratorBL.cs
@@ -0,0 +1,15 @@
+﻿using Domain;
+
+namespace IBusinessLogic
+{
+    public interface IAdministratorBL
+    {
+        public bool Authenticate(string email, string password);
+
+        public void AddAdministrator(Administrator administrator);
+
+        public void DeleteAdministrator(int id);
+
+        public void UpdateAdministrator(int id, Administrator administrator);
+    }
+}
