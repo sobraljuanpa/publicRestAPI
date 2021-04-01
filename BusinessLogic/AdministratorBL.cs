c5a6a3a3a423176ec0990697c887ee5bd1aa512a
Juan Pablo Sobral <sobraljuanpa@gmail.com>
2021-04-01 18:36:12 -0300

    GREEN All AdministratorBL tests passing

---
+++ BusinessLogic/AdministratorBL.cs
@@ -0,0 +1,40 @@
+﻿using Domain;
+using IBusinessLogic;
+using IDataAccess;
+
+namespace BusinessLogic 
+{
+
+    public class AdministratorBL : IAdministratorBL
+    {
+
+        private readonly IAdministratorRepository<Administrator> administratorRepository;
+
+        public AdministratorBL(IAdministratorRepository<Administrator> administratorRepository)
+        {
+            this.administratorRepository = administratorRepository;
+        }
+
+        public bool Authenticate(string email, string password)
+        {
+            return administratorRepository.Authenticate(email, password);
+        }
+
+        public void AddAdministrator(Administrator administrator)
+        {
+            administratorRepository.Add(administrator);
+        }
+
+        public void DeleteAdministrator(int id)
+        {
+            administratorRepository.Delete(id);
+        }
+
+        public void UpdateAdministrator(int id, Administrator administrator)
+        {
+            administratorRepository.Update(id, administrator);
+        }
+
+    }
+        
+}
