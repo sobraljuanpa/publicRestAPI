6bd4696b751c634e1f4a35afe738ee89d5a783fe
Juan Pablo Sobral <sobraljuanpa@gmail.com>
2021-04-03 21:47:35 -0300

    GREEN Added Controller class and GetContentById method

---
+++ WebAPI/Controllers/PlayableContentController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using IBusinessLogic;
+using Domain;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PlayableContentController : ControllerBase
+    {
+        private readonly IPlayerBL playerBL;
+
+        public PlayableContentController(IPlayerBL playerBL)
+        {
+            this.playerBL = playerBL;
+        }
+
+        //GET: /api/playablecontent/{id}
+        [HttpGet("{id}")]
+        public IActionResult GetContentById(int id)
+        {
+            PlayableContent content = playerBL.GetPlayableContent(id);
+            return Ok(content);
+        }
+    }
+}
