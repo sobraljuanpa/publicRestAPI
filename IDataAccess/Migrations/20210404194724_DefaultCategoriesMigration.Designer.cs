f618378359e0ae9f4a24b98de80035d1836c6421
Juan Pablo Sobral <sobraljuanpa@gmail.com>
2021-04-04 16:49:32 -0300

    Added default categories creation to context class and migrations

--- IDataAccess/Migrations/20210404194724_DefaultCategoriesMigration.Designer.cs
+++ IDataAccess/Migrations/20210404194724_DefaultCategoriesMigration.Designer.cs
@@ -4,14 +4,16 @@ using IDataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace IDataAccess.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20210404194724_DefaultCategoriesMigration")]
+    partial class DefaultCategoriesMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
