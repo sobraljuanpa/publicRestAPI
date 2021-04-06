682b0a9a62c3b9f7fb5b306960ea9e19fa1ed581
Chiara <chiara_dimarco@hotmail.com>
2021-04-06 15:46:05 -0300

    Added class PlaylistController

---
+++ WebAPI/Controllers/PlaylistController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using IBusinessLogic;
+using Domain;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PlaylistController : ControllerBase
+    {
+        private readonly IPlayerBL playerBL;
+    }
+}
+
