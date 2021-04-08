9f07460aef9b934db083055b305e5dec06c973e5
Juan Pablo Sobral <sobraljuanpa@gmail.com>
2021-04-08 17:23:57 -0300

    Added authentication support to services and configuration

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -11,11 +11,9 @@ using DataAccess;
 using IBusinessLogic;
 using BusinessLogic;
 using Domain;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 namespace WebAPI
 {
@@ -43,6 +41,25 @@ namespace WebAPI
             services.AddScoped<IRepository<Category>, CategoryRepository>();
             services.AddScoped<IRepository<PlayableContent>, PlayableContentRepository>();
             services.AddScoped<IPlayerBL, PlayerBL>();
+            //esta parte es la de autenticacion
+            var key = Encoding.ASCII.GetBytes("Ahora este es nuestro secreto.");
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +69,13 @@ namespace WebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors(x => x
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+
+            app.UseAuthentication();
 
             app.UseHttpsRedirection();
 
