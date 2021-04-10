7f43594dc81e988236683cae2acb27201813b6f4
Juan Pablo Sobral <sobraljuanpa@gmail.com>
2021-04-09 23:18:24 -0300

    GREEN Implemented CategoriesController

---
+++ WebAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using IBusinessLogic;
+
+namespace WebAPI.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly IPlayerBL playerBL;
+
+        public CategoriesController(IPlayerBL playerBL)
+        {
+            this.playerBL = playerBL;
+        }
+
+        [HttpGet()]
+        public IActionResult GetCategories()
+        {
+            try
+            {
+                var categories = playerBL.GetCategories();
+                return Ok(categories);
+            }
+            catch (Exception e)
+            {
+                return Problem(e.Message);
+            }
+
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCategoryContents(int id)
+        {
+            try
+            {
+                var contents = playerBL.GetCategoryElements(id);
+                return Ok(contents);
+            }
+            catch (Exception e)
+            {
+                return NotFound(e.Message);
+            }
+        }
+
+    }
+}
