b1dc6f4b50fb6def275bfea86f70450dae55eaca
Chiara <chiara_dimarco@hotmail.com>
2021-04-16 21:17:52 -0300

    Included missing [FromBody] in Administrator controller

--- WebAPI/Controllers/AdministratorsController.cs
+++ WebAPI/Controllers/AdministratorsController.cs
@@ -47,7 +47,7 @@ namespace WebAPI.Controllers
         }
 
         [HttpPost()]
-        public IActionResult AddAdministrator (Administrator administrator)
+        public IActionResult AddAdministrator ([FromBody] Administrator administrator)
         {
             try
             {
@@ -75,7 +75,7 @@ namespace WebAPI.Controllers
         }
 
         [HttpPost("{id}")]
-        public IActionResult UpdateAdministrator (int id, Administrator admin)
+        public IActionResult UpdateAdministrator (int id, [FromBody] Administrator admin)
         {
             try
             {
