77777d6d96f19bb885027aed927cc41331ca6ef5
Chiara <chiara_dimarco@hotmail.com>
2021-04-23 20:22:30 -0300

    Created PlaylistValidator class and changed affected classes

--- BusinessLogic/PlayableContentValidator.cs
+++ BusinessLogic/PlayableContentValidator.cs
@@ -37,5 +37,13 @@ namespace BusinessLogic
                 }
             }
         }
+
+        public void Exists (PlayableContent content)
+        {
+            if (content == null)
+            {
+                throw new Exception("There's no playable content associated to given id");
+            }
+        }
     }
 }
---
+++ BusinessLogic/PlaylistValidator.cs
@@ -0,0 +1,66 @@
+﻿using System.Linq;
+using System;
+
+using IDataAccess;
+using Domain;
+
+namespace BusinessLogic
+{
+    public class PlaylistValidator
+    {
+        private readonly IRepository<Playlist> playlistRepository;
+
+        public PlaylistValidator(IRepository<Playlist> repository)
+        {
+            playlistRepository = repository;
+        }
+        //public void IdInValidRange(int id)
+        //{
+        //    if (id <= 0 && id <= playlistRepository.GetAll().ToList().FindLast(x => x.Id != 0).Id)
+        //    { }
+        //    else
+        //    {
+        //        throw new Exception("No playlist associated to given id");
+        //    }
+        //}
+
+        public void IdInValidRange(int id)
+        {
+            if (id <= 0 && id > playlistRepository.GetAll().ToList().Count())
+            {
+                throw new Exception("No playlist associated to given id");
+            }
+        }
+
+        public void Exists(Playlist playlist)
+        {
+            if (playlist == null)
+            {
+                throw new Exception("There's no playlist associated to given id");
+            }
+        }
+
+        public void AlreadyOnPlaylist (Playlist playlist, PlayableContent content)
+        {
+            foreach (PlayableContent auxContent in playlist.Contents)
+            {
+                if (content.Name == auxContent.Name && content.Author == auxContent.Author)
+                {
+                    throw new Exception($"The playable content you are trying to add already exists at index {auxContent.Id}");
+                }
+
+            }
+        }
+
+        public void ValidPlaylist (Playlist playlist)
+        {
+            foreach (Playlist auxPlaylist in playlistRepository.GetAll().ToList())
+            {
+                if (playlist.Name == auxPlaylist.Name && playlist.Description == auxPlaylist.Description)
+                {
+                    throw new Exception($"The playlist you are trying to create already exists at index {auxPlaylist.Id}");
+                }
+            }
+        }
+    }
+}
