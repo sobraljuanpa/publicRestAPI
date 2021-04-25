709b161d51fee3ee31e8a9515a11a9ce070d020c
Chiara <chiara_dimarco@hotmail.com>
2021-04-25 00:40:37 -0300

    Added ConsultationBL interface

---
+++ IBusinessLogic/IConsultationBL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Domain;
+
+namespace IBusinessLogic
+{
+    public interface IConsultationBL
+    {
+        public List<Consultation> GetConsultationsByPsychologist();
+
+        public Consultation Get(int id);
+
+        public Consultation CreateConsultation(Consultation consultation);
+    }
+}
