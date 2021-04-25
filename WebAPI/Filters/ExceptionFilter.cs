c31cfb8b2239fcbe55e3d79a8b331ded91c7d51c
Chiara <chiara_dimarco@hotmail.com>
2021-04-24 23:21:05 -0300

    Created Filters folder and added class ExceptionFilter

---
+++ WebAPI/Filters/ExceptionFilter.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+
+namespace WebAPI.Filters
+{
+    public class ExceptionFilter : Attribute, IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
