ea647bfba6b2fd987c7b1d7bf21c8e1f98464aa7
Chiara <chiara_dimarco@hotmail.com>
2021-04-26 02:18:13 -0300

    Changed regular expression

--- BusinessLogic/ConsultationValidator.cs
+++ BusinessLogic/ConsultationValidator.cs
@@ -120,7 +120,7 @@ namespace BusinessLogic
 
         public void ValidRemoteAddress(String address)
         {
-            string format = @"\A[https]+(\://)[betterCalm]+(\.)[com]+(\.)[uy]+(\/)[meeting_id]+(\/)[codigo]";
+            string format = @"\A[https]+(\://)[betterCalm]+(\.)[com]+(\.)[uy]+(\/)[meeting_id]+(\/)[a-z0-9]";
             if (!Regex.Match(address, format).Success)
             {
                 throw new Exception("Address with wrong format.");
