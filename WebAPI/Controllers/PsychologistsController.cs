23a7b18bfa3cdc85dde9cd32caa390cc3604cb04
Chiara <chiara_dimarco@hotmail.com>
2021-05-04 22:35:40 -0300

    GREEN Implemented method

--- WebAPI/Controllers/PsychologistsController.cs
+++ WebAPI/Controllers/PsychologistsController.cs
@@ -47,19 +47,34 @@ namespace WebAPI.Controllers
         }
 
         [HttpGet("{id}")]
-        public IActionResult GetPsychologistById (int id)
+        public IActionResult GetPsychologistById(int id)
         {
             try
             {
                 Psychologist psychologist = psychologistBL.GetPsychologist(id);
                 return Ok(psychologist);
-            } 
+            }
             catch (Exception e)
             {
                 return NotFound(e.Message);
             }
 
         }
+
+        [HttpPost("{id}/problems")]
+        public IActionResult AddProblemToPsychologist(int id, [FromBody] Psychologist psychologist)
+        {
+            try
+            {
+                psychologistBL.AddProblemToPsychologist(psychologist, id);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
 
         [HttpPost("{id}")]
         public IActionResult UpdatePsychologist (int id, [FromBody] Psychologist psychologist)
