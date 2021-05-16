52da93df7ce98352147cc197d47b46ccba840217
Juan Pablo Sobral <sobraljuanpa@gmail.com>
2021-05-16 11:39:05 -0300

    updated project structure

--- bettercalm-frontend/src/app/app.module.ts
+++ bettercalm-frontend/src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { CategoryComponent } from './category/category.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CategoryComponent } from './components/category/category.component';
 
 @NgModule({
   declarations: [
