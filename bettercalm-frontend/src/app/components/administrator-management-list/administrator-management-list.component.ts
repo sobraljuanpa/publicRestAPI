c3050d1b63b2330815b333f15daf8a55f0743af8
Juan Pablo Sobral <sobraljuanpa@gmail.com>
2021-05-24 19:35:22 -0300

    Display administrator list working

--- bettercalm-frontend/src/app/components/administrator-management-list/administrator-management-list.component.ts
+++ bettercalm-frontend/src/app/components/administrator-management-list/administrator-management-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Administrator } from 'src/app/models/administrator';
+import { AdministratorService } from 'src/app/services/administrator.service';
 
 @Component({
   selector: 'app-administrator-management-list',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdministratorManagementListComponent implements OnInit {
 
-  constructor() { }
+  administrators!: Administrator[];
+
+  constructor(private administratorService: AdministratorService) { }
 
   ngOnInit(): void {
+    this.getAdministrators();
+  }
+
+  getAdministrators() {
+    this.administratorService.getAdministrators()
+    .subscribe(administrators => this.administrators = administrators);
+  }
+
+  Delete(id: number) {
+    console.log(id);
   }
 
 }
--- bettercalm-frontend/src/app/services/administrator.service.ts
+++ bettercalm-frontend/src/app/services/administrator.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { Administrator, AdministratorAdapter } from '../models/administrator';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +11,9 @@ export class AdministratorService {
 
   private adminURL = 'http://localhost:5000/api/administrators';
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private adapter: AdministratorAdapter) { }
 
   addAdministrator(email: string, name: string, password: string) {
     return this.http.post(this.adminURL, {
@@ -16,5 +21,12 @@ export class AdministratorService {
       Name: name,
       Password: password
     });
+  }
+
+  getAdministrators() : Observable<Administrator[]> {
+    return this.http.get<Administrator[]>(this.adminURL)
+    .pipe(
+      map((data: any[]) => data.map(item => this.adapter.adapt(item)))
+    );
   }
 }
