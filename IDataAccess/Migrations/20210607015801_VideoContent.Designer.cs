e49c94d46ceb61122a156ad07c6bfaa20abc1f9f
Juan Pablo Sobral <sobraljuanpa@gmail.com>
2021-06-06 23:00:47 -0300

    Added migration for supporting video content

--- IDataAccess/Migrations/20210607015801_VideoContent.Designer.cs
+++ IDataAccess/Migrations/20210607015801_VideoContent.Designer.cs
@@ -4,14 +4,16 @@ using IDataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace IDataAccess.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20210607015801_VideoContent")]
+    partial class VideoContent
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
