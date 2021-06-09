b62b21df77419f7585d67b59b4a8ee2b6c937c26
Chiara <chiara_dimarco@hotmail.com>
2021-06-09 11:28:42 -0300

    New importation class in BusinessLogic

---
+++ BusinessLogic/Importation.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Domain;
+using IBusinessLogic;
+
+namespace BusinessLogic
+{
+    public class Importation : IImportationLogic
+    {
+        IImportation _importation;
+        PlayerBL _playerBL;
+        Playlist playlist = null;
+        PlayableContent playableContent = null;
+
+        public Importation(IImportation importation, PlayerBL playerBL)
+        {
+            _importation = importation;
+            _playerBL = playerBL;
+        }
+
+        public void AddPlayableContent()
+        {
+            try
+            {
+                playableContent = _importation.GetPlayableContent();
+                _playerBL.AddIndependentContent(playableContent);
+            }
+            catch(Exception e)
+            {
+                throw new Exception("..");
+            }
+        }
+
+        public void AddPlaylist()
+        {
+            try
+            {
+                playlist = _importation.GetPlaylist();
+                _playerBL.AddPlaylist(playlist);
+            }
+            catch(Exception e)
+            {
+                throw new Exception("..");
+            }
+        }
+
+    }
+}
