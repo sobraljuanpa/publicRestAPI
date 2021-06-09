58bd937c5aa9b7be550fc247fbb41c66eab240b9
Chiara <chiara_dimarco@hotmail.com>
2021-06-08 22:45:10 -0300

    Possible interface for new importation requirement

---
+++ IBusinessLogic/IImportation.cs
@@ -0,0 +1,21 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IBusinessLogic
+{
+    public interface IImportation
+    {
+        public String GetImportationName();
+
+        public List<object> GetParameters();
+
+        public List<PlayableContent> GetPlayableContents();
+
+        public List<Playlist> GetPlaylists();
+
+        public List<object> ImportContent(List<object> parameters);
+
+    }
+}
