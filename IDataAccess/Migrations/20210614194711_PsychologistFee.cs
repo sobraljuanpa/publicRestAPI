bfca235d02924743323689f3076eb910fc5aceb4
Juan Pablo Sobral <sobraljuanpa@gmail.com>
2021-06-14 16:49:03 -0300

    Added migration for fee in psyychologist

---
+++ IDataAccess/Migrations/20210614194711_PsychologistFee.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IDataAccess.Migrations
+{
+    public partial class PsychologistFee : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Fee",
+                table: "Psychologists",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Bonus",
+                table: "Consultations",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Cost",
+                table: "Consultations",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Duration",
+                table: "Consultations",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Fee",
+                table: "Psychologists");
+
+            migrationBuilder.DropColumn(
+                name: "Bonus",
+                table: "Consultations");
+
+            migrationBuilder.DropColumn(
+                name: "Cost",
+                table: "Consultations");
+
+            migrationBuilder.DropColumn(
+                name: "Duration",
+                table: "Consultations");
+        }
+    }
+}
