d82d3bde4ac2c15ec7d49dbf997001483d691d16
Chiara <chiara_dimarco@hotmail.com>
2021-06-14 22:57:03 -0300

    Created new DTO for Consultation

---
+++ Domain/DTOs/ConsultationDTO.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.DTOs
+{
+    public class ConsultationDTO
+    {
+        public int Id { get; set; }
+
+        public string PatientName { get; set; }
+
+        public DateTime PatientBirthDate { get; set; }
+
+        public string PatientEmail { get; set; }
+
+        public string PatientPhone { get; set; }
+
+        public int ProblemId { get; set; }
+
+        public int PsychologistId { get; set; }
+
+        public bool IsRemote { get; set; }
+
+        public string Address { get; set; }
+
+        public int Date { get; set; }
+
+        public int Duration { get; set; }
+
+        public int Bonus { get; set; }
+
+        public int Cost { get; set; }
+    }
+}
