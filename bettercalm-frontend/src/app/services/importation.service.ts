56f0a485a65dce4dd704d251b0ef1502722aa3a6
Chiara <chiara_dimarco@hotmail.com>
2021-06-16 11:32:03 -0300

    Created importation service

---
+++ bettercalm-frontend/src/app/services/importation.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse } from "@angular/common/http";
+import { GlobalVariables } from '../globals';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ImportationService {
+
+  private playablecontentURL = `${GlobalVariables.BASE_API_URL}/playablecontents`;
+  private videocontentURL = `${GlobalVariables.BASE_API_URL}/videos`;
+  private playlistURL = `${GlobalVariables.BASE_API_URL}/playlists`;
+
+  constructor(private http: HttpClient) { }
+
+  addPlayableContentImportation(type: string, path: string[]) {
+    debugger;
+    return this.http.post(`${this.playablecontentURL}/${type}`, path)
+  }
+}
